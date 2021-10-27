8a4c32a434282a1484bd72e9049efc9fe0d0de30
edsonCVN <edson.da.veiga@tecnico.ulisboa.pt>
2021-10-27 23:51:36 +0100

    nd d geito

--- ggc-skeleton/ggc/core/Product/Recipe.java
+++ ggc-skeleton/ggc/core/Product/Recipe.java
@@ -2,17 +2,21 @@ package ggc.core.Product;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Locale;
 import java.text.Collator;
+
+import ggc.core.Product.Component;
 
 public class Recipe {
 
 	private double _alpha;
-	private List<Component> _components;
+	private List<Component> _components = new ArrayList<>();
 
-	public Recipe(double alpha) {
+	public Recipe(double alpha, List<Component> components) {
 		_alpha = alpha;
+		_components = components;
 	}
 
 	public double getAlpha() { return _alpha; }
@@ -23,6 +27,12 @@ public class Recipe {
 
 	@Override
 	public String toString() {
-		return "|";
+		String recipe;
+		for (int i = 0; i < _alpha; i++) {
+			if(i != 0)
+				recipe += "#";
+			recipe += _components[i].toString;
+		}
+		return "|" + recipe;
 	}
 }
