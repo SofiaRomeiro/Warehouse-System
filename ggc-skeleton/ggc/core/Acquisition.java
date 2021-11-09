d0c622aafd003ac4e046ba7831bbd9c25245c7c4
edsonCVN <edson.da.veiga@tecnico.ulisboa.pt>
2021-11-09 18:32:26 +0000

    transacoes

--- ggc-skeleton/ggc/core/Acquisition.java
+++ ggc-skeleton/ggc/core/Acquisition.java
@@ -2,18 +2,18 @@ package ggc.core;
 
 public class Acquisition extends Transaction {
     
-    public Acquisition(int id, int date, double baseValue, int quantity, Product product, Partner partner) {
-        super(id, date, baseValue, quantity, product, partner);
+    public Acquisition(int id, int transactionDate, double baseValue, int quantity, Product product, Partner partner) {
+        super(id, transactionDate, baseValue, quantity, product, partner);
     }
 
     public  boolean isPaid() {
         return true;
     }
     public  int getPaymentDate() {
-        return super.getDate();
+        return super.getTransactionDate();
     }
 
     public String toString() {
-        return "COMPRA|" + super.getId() + "|" + super.getPartner().getKey() + "|"  + super.getProduct().getId() + "|" + super.getQuantity() + "|" + getBaseValue() + "|" + super.getDate();
+        return "COMPRA|" + super.getId() + "|" + super.getPartner().getKey() + "|"  + super.getProduct().getId() + "|" + super.getQuantity() + "|" + getBaseValue() + "|" + super.getTransactionDate();
     }
 }
--- ggc-skeleton/ggc/core/BreakdownSale.java
+++ ggc-skeleton/ggc/core/BreakdownSale.java
@@ -3,19 +3,19 @@ package ggc.core;
 public class BreakdownSale extends Sale{
 
 
-    public BreakdownSale(int id, int date, double baseValue, int quantity, Product product, Partner partner) {
-        super(id, date, baseValue, quantity, product, partner);
+    public BreakdownSale(int id, int transactionDate, double baseValue, int quantity, Product product, Partner partner) {
+        super(id, transactionDate, baseValue, quantity, product, partner);
     } 
 
     public  boolean isPaid() {
         return true;
     }
     public  int getPaymentDate() {
-        return super.getDate();
+        return super.getTransactionDate();
     }
 
     public String toString() {
-        return "DESAGREGAÇÃO|" + super.getId() + "|" + super.getPartner().getKey() + "|"  + super.getProduct().getId() + "|" + super.getQuantity() + "|" + getBaseValue() + "|" + super.getValuePaid() + "|" + super.getDate() + "|" + super.getProduct();
+        return "DESAGREGAÇÃO|" + super.getId() + "|" + super.getPartner().getKey() + "|"  + super.getProduct().getId() + "|" + super.getQuantity() + "|" + getBaseValue() + "|" + super.getValuePaid() + "|" + super.getTransactionDate() + "|" + ((AggregateProduct) super.getProduct()).getRecipe();
     }
     
 }
