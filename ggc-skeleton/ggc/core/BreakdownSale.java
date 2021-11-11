dff3be280f0566be6bd8463b386e82ae67eebf74
edsonCVN <edson.da.veiga@tecnico.ulisboa.pt>
2021-11-11 21:01:13 +0000

    de

--- ggc-skeleton/ggc/core/BreakdownSale.java
+++ ggc-skeleton/ggc/core/BreakdownSale.java
@@ -1,22 +1,15 @@
 package ggc.core;
 
-import java.util.Map;
-import java.util.HashMap;
-
 public class BreakdownSale extends Sale{
 
-    private Map<String, Batch> _batches;
     public BreakdownSale(int id, Date transactionDate, double baseValue, int quantity, Product product, Partner partner) {
         super(id, transactionDate, baseValue, quantity, product, partner);
-        _batches = new HashMap<>();
-
     } 
 
     public  boolean isPaid() {
         return true;
     }
   
-
     public String toString() {
         return "DESAGREGAÇÃO|" + super.getId() + "|" + super.getPartner().getKey() + "|"  + super.getProduct().getId() + "|" + super.getQuantity() + "|" + Math.round(getBaseValue()) + "|" + Math.round(super.getValuePaid()) + "|" + super.getTransactionDate().getPaymentDate() + "|" + ((AggregateProduct) super.getProduct()).getRecipe();
     }
